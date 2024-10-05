1522129a0689152ba7776587d1b83e80862146f4
jjkool1214 <jaysenstolarz@gmail.com>
2024-10-05 01:22:15 -0400

    Pushing bubble class

---
+++ Do Not Disturb/Classes/Bubble.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_Not_Disturb.Classes
+{
+    internal class Bubble : Collidable
+    {
+        private Texture2D sprite;
+        private const float bubbleVelocityY = -40;
+        public Bubble(Vector2 position, Rectangle hitbox) : base(position, hitbox)
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public bool IsColliding(Collidable collidable)
+        {
+            if(this.hitbox.Intersects(collidable.Hitbox))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Pop(Collidable collidable)
+        {
+            collidabl
+        }
+
+
+    }
+}
